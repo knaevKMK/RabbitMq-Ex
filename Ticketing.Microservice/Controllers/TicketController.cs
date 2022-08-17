8344b03df84fc7e9f892bf772707c83c2d81e5a7
knaevKMK <71128514+knaevKMK@users.noreply.github.com>
2022-08-17 23:47:57 +0300

    initial create

---
+++ Ticketing.Microservice/Controllers/TicketController.cs
@@ -0,0 +1,41 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Ticketing.Microservice.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private readonly IBus _bus;
+
+        public TicketController(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        [HttpGet("/")]
+        public async Task<ActionResult> Ping()
+        {
+            await Task.CompletedTask;
+            return Ok("Pong");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTicket(Ticket ticket)
+        {
+            if (ticket != null)
+            {
+                ticket.BookedOn = DateTime.Now;
+                Uri uri = new Uri("rabbitmq://localhost/ticketQueue");
+                var endPoint = await _bus.GetSendEndpoint(uri);
+                await endPoint.Send(ticket);
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
